27da32b62cfc4922980c72b8dec50d8334aaef57
이슬기 <tmfrl1552@naver.com>
2020-09-05 16:21:17 +0900

    api_book model update

--- bookey_restapi/api_book/models.py
+++ bookey_restapi/api_book/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Book(models.Model):
     bid = models.CharField(max_length=20, primary_key=True)
     name = models.CharField(max_length=30, null=False)
-    author = models.CharField(max_length=30, null=False)
+    author = models.CharField(max_length=50, null=False)
     publisher = models.CharField(max_length=30, null=False)
     price = models.CharField(max_length=20, null=False)
     list = models.TextField(null=False)
