64fdf985d36fb0ea8b93ae202ec9e667b87849ac
이슬기 <tmfrl1552@naver.com>
2020-09-17 02:54:23 +0900

    update api_result model

--- bookey_restapi/api_result/models.py
+++ bookey_restapi/api_result/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Result(models.Model):
     bid = models.ForeignKey('api_book.Book', on_delete=models.CASCADE, db_column='bid')
-    good = models.CharField(max_length=10)
+    good = models.IntegerField()
 
     class Meta:
         db_table = 'Result'
